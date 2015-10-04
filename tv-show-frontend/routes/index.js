1d7a89d2e44066fdfd121122a7c3308ee92994e1
Priyank Jain <priyankjjain61@gmail.com>
2015-10-04 13:21:53 +0530

    Added language tags mapping

--- tv-show-frontend/routes/index.js
+++ tv-show-frontend/routes/index.js
@@ -12,11 +12,25 @@ router.get('/suits', function(req, res){
 	var db = req.db;
 	var collection = db.get('all_stats');
 	var astats = "";
+	var sstats = "";
 	collection.findOne({"tvshow":"suits"},{},function(e, docs){
 		astats = docs;
-		res.render('suits',{
-			"all_stats": astats
-		})
+		sc = db.get('source_stats');
+		sc.find({"tvshow":"suits"},{"sort":[["source_count","desc"]], "limit":10},function(e,docs){
+			sstats = docs;
+			lc = db.get('lang_stats');
+			lc.find({"tvshow":"suits"},{"sort":[["lang_count","desc"]], "limit":10},function(e,docs){
+				lstats = docs;
+				res.render('suits',{
+					"all_stats": astats,
+					"source_stats" : sstats,
+					"lang_stats": lstats
+				})
+			});
+		});
 	});
+
+
+	
 });
 module.exports = router;
